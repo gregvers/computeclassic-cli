d4dcb380aa9a9ade803cf5315085531be5d9d3b8
gregvers <gregvers@gmail.com>
2018-02-08 14:53:02 -0800

    update for DNS support

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # computeclassic-cli
 docker image to run Oracle Compute Classic CLI
 
+## BUILD
 Download the CLI installation bundle from OTN (http://www.oracle.com/technetwork/topics/cloud/downloads/opc-cli-3096036.html) and place the zip file in the same folder as the Dockerfile
 
-BUILD:
 docker build --build-arg IDENTITYDOMAIN=lalala --build-arg USER=greg@oracle.com --build-arg PASSWORD="XXX" --build-arg COMPUTE_ENDPOINT=https://endpoint/ -t gregvers/computeclassic-cli .
 
-RUN:
+## RUN
 docker run -it --env-file config.env gregvers/computeclassic-cli
 
-config.env example:
+## config.env example:
 USER=gre@domain.com
 PASSWORD=XXXXXXX
 IDENTITYDOMAIN=myIdentityDomain
--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# IDENTITYDOMAIN, ENDPOINT, USER and PASSWORD are set
+# IDENTITYDOMAIN, COMPUTE_ENDPOINT, USER and PASSWORD are set
+# DNS1 and DNS2 are optional
 
 mkdir -p /root/.opc/profiles/
 cat /root/.opc/profiles/profile-template.json | sed "s|USER|$USER|g" | sed "s|COMPUTE_ENDPOINT|${COMPUTE_ENDPOINT#*//}|g" | sed "s|IDENTITYDOMAIN|$IDENTITYDOMAIN|g" > /root/.opc/profiles/profile.json
 echo $PASSWORD > /root/.opc/passwordfile
 chmod -R 600 /root/.opc
+
+if [ "$DNS1" != "" ]
+then
+  echo "nameserver" $DNS1 > /etc/resolv.conf
+  if [ "$DNS2" != "" ]
+  then
+    echo "nameserver" $DNS2 >> /etc/resolv.conf
+  fi
+fi
 
 echo 'PS1="/Compute-$IDENTITYDOMAIN/$USER  \w\$ "' >> /root/.bashrc
 
